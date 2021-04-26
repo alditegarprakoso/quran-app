ffdde5e44bf6bc067ef7ff8361db97dac80c2953
Aldi Tegar Prakoso <alditegarprakoso@gmail.com>
2021-04-26 16:59:31 +0700

    membuat fungsi cari surah

--- src/views/Quran.vue
+++ src/views/Quran.vue
@@ -1,10 +1,34 @@
 <template>
   <div class="quran mt-5">
-    <div class="row row-cols-1 row-cols-md-3">
-      <div class="col mb-4" v-for="surah in data_surah" :key="surah.number">
-        <router-link :to="'/quran/' + surah.number" class="surah-detail">
-          <CardSurah :surah="surah" />
-        </router-link>
+    <div class="row mb-4">
+      <div class="col">
+        <div class="form-group">
+          <input
+            v-model="search"
+            type="text"
+            class="form-control"
+            placeholder="Cari Surah..."
+            @keyup="getSuratNumberByName(search)"
+          />
+        </div>
+      </div>
+    </div>
+    <div v-if="data_search">
+      <div class="row row-cols-1 row-cols-md-3">
+        <div class="col mb-4" v-for="surah in data_search" :key="surah.number">
+          <router-link :to="'/quran/' + surah.number" class="surah-detail">
+            <CardSurah :surah="surah" />
+          </router-link>
+        </div>
+      </div>
+    </div>
+    <div v-else>
+      <div class="row row-cols-1 row-cols-md-3">
+        <div class="col mb-4" v-for="surah in data_surah" :key="surah.number">
+          <router-link :to="'/quran/' + surah.number" class="surah-detail">
+            <CardSurah :surah="surah" />
+          </router-link>
+        </div>
       </div>
     </div>
     <div id="top">
@@ -25,11 +49,29 @@ export default {
   data() {
     return {
       data_surah: {},
+      search: "",
+      data_search: null,
     };
   },
   methods: {
     getSurah(data) {
       this.data_surah = data;
+    },
+    getSuratNumberByName(surahName) {
+      const surahFilter = this.data_surah.filter((item) =>
+        item.name.transliteration.id.includes(surahName)
+      );
+
+      try {
+        if (!surahFilter) {
+          return this.data_surah;
+        } else {
+          this.data_search = surahFilter;
+          return this.data_search;
+        }
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
   mounted() {
--- src/views/QuranDetail.vue
+++ src/views/QuranDetail.vue
@@ -20,37 +20,60 @@
       </div>
     </div>
 
-    <table class="table table-striped table-inverse table-responsive" style="">
-      <tbody>
-        <tr v-for="surah in detail_surah.verses" :key="surah.number">
-          <td>
-            <div class="row mb-3">
-              <div class="col-2 arabic text-center">
+    <div class="row justify-content-center mb-5">
+      <div class="col-md-10">
+        <table class="table table-striped table-inverse table-responsive">
+          <tbody>
+            <tr v-if="detail_surah.preBismillah != null">
+              <div class="row mb-3">
                 <div
-                  style="
-                    width: 30px;
-                    height: 30px;
-                    border: 1.5px solid black;
-                    border-radius: 50%;
-                    font-weight: 700;
-                  "
+                  class="col-12 lh arabic text-right"
+                  style="font-size: 25px"
                 >
-                  {{ surah.number.inSurah }}
+                  {{ detail_surah.preBismillah.text.arab }}
                 </div>
               </div>
-              <div class="col-10 lh arabic text-right" style="font-size: 25px">
-                {{ surah.text.arab }}
+              <div class="row mb-2">
+                <div class="col-10 ml-2">
+                  {{ detail_surah.preBismillah.translation.id }}
+                </div>
               </div>
-            </div>
-            <div class="row">
-              <div class="col-12">
-                {{ surah.translation.id }}
-              </div>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+            </tr>
+            <tr v-for="surah in detail_surah.verses" :key="surah.number">
+              <td>
+                <div class="row mb-3">
+                  <div class="col-2 arabic text-center">
+                    <div
+                      style="
+                        width: 30px;
+                        height: 30px;
+                        border: 1.5px solid black;
+                        border-radius: 50%;
+                        font-weight: 700;
+                      "
+                    >
+                      {{ surah.number.inSurah }}
+                    </div>
+                  </div>
+                  <div
+                    class="col-10 lh arabic text-right"
+                    style="font-size: 25px"
+                  >
+                    {{ surah.text.arab }}
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-12">
+                    {{ surah.translation.id }}
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
     <div id="top">
       <a href="#atas"><b-icon-arrow-up></b-icon-arrow-up></a>
     </div>
